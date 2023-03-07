048ed312485539c74a77552b3df6abd5f5619ada
Alexandra Chernova <estelelendil@mail.ru>
2023-03-07 12:03:29 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card title='keep calm' text='and drink wine' link='https://www.wildberries.ru/catalog/13052675/detail.aspx' >
+        <image
+          //  src="https://m.media-amazon.com/images/I/41-13-Uq4-L._AC_SY450_.jpg"
+          class="card-img-top" alt="альтернативная картинка"></image>
+
+      </Card>
+      <Card title='keep calm' text='and drink wine' link='https://www.wildberries.ru/catalog/13052675/detail.aspx' >
+
+
+      </Card>
     </div>
   );
 }
